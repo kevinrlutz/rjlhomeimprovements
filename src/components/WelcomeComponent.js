ff65c1a6e221625b8d86d170bb4739434517a39b
Kevin Lutz <kevinlutz429@gmail.com>
2025-03-07 19:19:56 -0500

    Added project image gallery, favicon, fixes

--- src/components/WelcomeComponent.js
+++ src/components/WelcomeComponent.js
@@ -8,7 +8,7 @@ export default function WelcomeComponent() {
         <h1 className='centered'>"We take pride in our work"</h1>
         <br />
         <p>
-        Robert J Lutz Home Improvements has been a trusted contractor dedicated to providing top-quality home improvement services since 2004. 
+        Robert J Lutz Home Improvements has been a trusted contractor dedicated to providing top-quality home improvement services since 2006. 
         Owned and operated by a lifelong Orange County resident, we pride ourselves on delivering high-quality craftsmanship and personalized service. We are 
         fully licensed contractors proudly serving the communities of Orange, Rockland, Dutchess, and Westchester counties. Whether it's a small 
         repair or a major renovation, we ensure that each job is completed with the utmost attention to detail and customer satisfaction. Let us help you 
